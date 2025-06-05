080d14058e28cd8c267ddc3e0fd0b4d48076e608
kkranthireddy6 <kkranthireddy6@gmail.com>
2025-06-04 20:39:49 -0500

    Update flatObject.js

--- flatObjectData/flatObject.js
+++ flatObjectData/flatObject.js
@@ -11,9 +11,32 @@
 };
 
 
-//output: 
+//output: ["Suneel", "Hyderabad", "Chennai", "AT&T", "AT&T1", 1234, 5678, false]
+//Method 1: Dynamic code
     const output = Object.values(inputData).flatMap(value=>
         typeof value === 'object' ? Object.values(value) : value
       )
     
     console.log(output)
+
+//Method 2: 
+  const outputArray = [
+  inputData.name,
+  ...inputData.location,
+  ...Object.values(inputData.office),
+  ...inputData.pin,
+  inputData.working
+]
+
+console.log(outputArray)
+
+//Method 3:
+  const result = []
+  
+  result.push(inputData.name)
+  inputData.location.map((loc)=> result.push(loc))
+  Object.values(inputData.office).map(ofce=> result.push(ofce))
+  inputData.pin.map(pin=>result.push(pin))
+  result.push(inputData.working)
+  
+  console.log(result)
