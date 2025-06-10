0690bd6a89e482764a2499cfc6b6d693144c736d
kkranthireddy6 <kkranthireddy6@gmail.com>
2025-06-10 11:21:23 -0500

    Update filterandindexOf.js

--- Remove Duplicates from Array/filterandindexOf.js
+++ Remove Duplicates from Array/filterandindexOf.js
@@ -1,13 +1,10 @@
 const duplicateArr = [1, 2, 1, 3, 4, 2, 2, 1, 5, 6];
 
-let uniqueArr = duplicateArr.filter((item, index)=> duplicateArr.indexOf(item) === index)
-
-console.log(uniqueArr)
-
-
-const set: Set<number> = new Set(numbers); // correct type
+const set = new Set(numbers); // correct type
 console.log([...set]);
 
-
-const uniqueNumbers: number[] = [...new Set(numbers)];
+const uniqueNumbers = [...new Set(numbers)];
 console.log(uniqueNumbers);
+
+let uniqueArr = duplicateArr.filter((item, index)=> duplicateArr.indexOf(item) === index)
+console.log(uniqueArr)
