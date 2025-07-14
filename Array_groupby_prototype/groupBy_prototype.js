c91e74f2db85310f9c9deda6b5891b1781d09b2d
kkranthireddy6 <kkranthireddy6@gmail.com>
2025-07-14 17:33:25 -0500

    Update groupBy_prototype.js

--- Array_groupby_prototype/groupBy_prototype.js
+++ Array_groupby_prototype/groupBy_prototype.js
@@ -14,13 +14,30 @@ Array.prototype.groupBy = function () {
     return group
   }, {})
 }
-
 console.log(people.groupBy())
 
+//groupBy prototype generic function
+Array.prototype.groupBy = function(key) {
+  return this.reduce((group, item) => {
+    const groupKey = item[key];
+    if (!group[groupKey]) {
+      group[groupKey] = [];
+    }
+    group[groupKey].push(item);
+    return group;
+  }, {});
+};
+const people = [
+  { name: 'Kelly', age: 20 },
+  { name: 'Sally', age: 21 },
+  { name: 'John', age: 22 },
+  { name: 'Kelly', age: 20 }
+]
+
+console.log(people.groupBy('name'))
 
 //HashMaps groupBy
 const useMaps = new Map()
-
 for(const {name, age} of people){
   useMaps.set(name, (useMaps.get(name)||0)+age)
 }
